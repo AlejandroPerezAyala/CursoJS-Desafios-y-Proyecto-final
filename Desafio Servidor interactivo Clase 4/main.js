540b375d2646fe3380df200ff003515893409fb2
Alejandro <perezalejandro266@gmail.com>
2022-05-22 14:05:22 -0300

    Servidor interactivo Clase 4

--- Desafio Servidor interactivo Clase 4/main.js
+++ Desafio Servidor interactivo Clase 4/main.js
@@ -1,24 +1,29 @@
-
+//Declaracion de variables 
 let nombre;
 let apellido;
 
-nombre = prompt("ingrese su nombre:");
-apellido = prompt("ingrese su apellido:");
-
-if (!nombre.match("[A-Za-z]") && !apellido.match("[A-Za-z]")) {
-    do {
-        alert("no ingreso un nombre o apellido valido!");
-        nombre = prompt("ingrese nuevamente su nombre:");
-        apellido = prompt("ingrese nuevamente su apellido:");
-    } while (!nombre.match("[A-Za-z]") && !apellido.match("[A-Za-z]"));
+//Declaracion de funciones
+//Funcion para validar que solo se ingresen letras
+function validarString (){
+    if (!nombre.match("[A-Za-z]") && !apellido.match("[A-Za-z]")) {
+        do {
+            alert("no ingreso un nombre o apellido valido!");
+            nombre = prompt("ingrese nuevamente su nombre:");
+            apellido = prompt("ingrese nuevamente su apellido:");
+        } while (!nombre.match("[A-Za-z]") && !apellido.match("[A-Za-z]"));
+    }
 }
-
-function saludo(nombre, apellido){
+//funcion para saludar
+function saludo(){
     alert("Bienvenido a Clouds Accesorios: " + nombre + " " + apellido)
 }
 
-if (nombre.match("[A-Za-z]") && apellido.match("[A-Za-z]")) {
-    saludo(nombre, apellido);
-}
-    
+//Asignacion de variables
+nombre = prompt("ingrese su nombre:");
+apellido = prompt("ingrese su apellido:");
 
+if (nombre.match("[A-Za-z]") && apellido.match("[A-Za-z]")) {
+    saludo();
+} else{
+    validarString();
+}
